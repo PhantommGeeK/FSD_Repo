21c6b2b8a592574e2f13b5bb672ad04ddf17acab
PhantommGeeK <devansh.22b1541054@abes.ac.in>
2024-10-15 15:38:59 +0530

    day 5

---
+++ Day5/script.js
@@ -0,0 +1,21 @@
+// let x =confirm("Are you sure to logout");
+// alert(x);
+
+// let v =prompt("Enter your name","Rahul");
+// alert(v);
+
+// let disp = () =>{
+//     console.log("Welcome to arrow function in JS!")
+// };
+// disp()
+
+// let say =()=> console.log("Single line arrow function")
+// say()
+
+
+let sum =(x,y)=>{
+    return x+y;
+}
+
+let res = sum(12,32);
+console.log(res);
